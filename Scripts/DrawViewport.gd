1e87b69b7ba551304a126cf49169739e3407756f
Boon <koryuboon@gmail.com>
2023-12-03 18:00:29 +0000

    Added completed cloth counter and a timer.

--- Scripts/DrawViewport.gd
+++ Scripts/DrawViewport.gd
@@ -2,20 +2,36 @@ extends SubViewport
 
 class_name DrawViewport
 
-signal progress_above_threshold
+signal progress_above_threshold()
 
 @export var brush: Sprite2D
 @export var cal_vp: CalculationViewport
 @export var progress_label: Label3D
+@export var time_label: Label3D
 @export var black_bg: ColorRect
 
 @export var threshold: int = 90
+
+var time_elapsed: float = -1
+
+func _process(delta):
+	if time_elapsed < 0:
+		return
+	time_elapsed += delta
+	var seconds_rounded = roundi(time_elapsed)
+	var minutes = seconds_rounded / 60
+	var seconds = seconds_rounded % 60
+	time_label.text = "Time:\n%02d:%02d" % [minutes, seconds]
+	
 
 func move_brush(position: Vector2, brush_amount: float, first: bool = false):
 	render_target_update_mode = SubViewport.UPDATE_ONCE
 	brush.self_modulate.a = brush_amount
 	brush.position = position
 	if !first:
+		if time_elapsed < 0:
+			time_elapsed = 0
+		
 		black_bg.visible = false
 		
 		var progress = cal_vp.set_texture(get_texture())
@@ -24,6 +40,9 @@ func move_brush(position: Vector2, brush_amount: float, first: bool = false):
 	
 		if progress_rounded >= threshold:
 			progress_above_threshold.emit()
+	else:
+		time_elapsed = -1
+		time_label.text = "Time:\n00:00"
 
 func refresh():
 	black_bg.visible = true
--- Scripts/FabricSpawner.gd
+++ Scripts/FabricSpawner.gd
@@ -7,11 +7,16 @@ extends Node3D
 @export var spawn_wait_timer: Timer # timer to prevent spawning too frequently and infinite recursive (when threshold signal keep calling)
 @export var success_particle: GPUParticles3D
 
+@export var steamer: ClothesSteamer
+
+@export var completed_label: Label3D
+
 @export var fabric_color_texs: Array[Texture2D]
 @export var fabric_normal_texs: Array[Texture2D]
 
 var last_spawned_fabric_index: int = -1
 var spawning := false
+var completed_count: int = 0
 
 func _ready():
 	p2uv_converter.set_mesh(fabric)
@@ -37,6 +42,10 @@ func _on_draw_viewport_progress_above_threshold():
 	if !spawning:
 		spawn_new_fabric()
 		success_particle.emitting = true
+		completed_count += 1
+		completed_label.text = "Completed\nClothes:\n%d" % completed_count
+		steamer.set_pressing(false)
+		
 
 
 func _on_timer_timeout():
